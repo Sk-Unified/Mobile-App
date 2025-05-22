854864c5582d6691b778b9dfee24572dd57744a1
Sourabh Singh <sourabhsinghredmi@gmail.com>
2025-05-22 16:59:06 +0530

    Initial commit
    Generated by create-expo-stack 2.18.3

---
+++ navigation/index.tsx
@@ -0,0 +1,44 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { useUnistyles } from 'react-native-unistyles';
+
+import Overview from '../screens/overview';
+import Details from '../screens/details';
+import { BackButton } from '../components/BackButton';
+
+export type RootStackParamList = {
+  Overview: undefined;
+  Details: { name: string };
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+export default function RootStack() {
+  const { theme } = useUnistyles();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Overview"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: theme.colors.background,
+          },
+          headerTitleStyle: {
+            color: theme.colors.typography,
+          },
+          headerTintColor: theme.colors.typography,
+        }}>
+        <Stack.Screen name="Overview" component={Overview} />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={({ navigation }) => ({
+            headerLeft: () => <BackButton onPress={navigation.goBack} />,
+          })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
